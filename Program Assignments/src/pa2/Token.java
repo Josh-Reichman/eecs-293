00544b665fda3c2b94aa9535ddc41fef5a4403f5
Josh Reichman <whodaman101@gmail.com>
2017-09-11 13:29:27 -0400

    added more to token.java
    Override hashcode and equals functions, LocationalToken class

--- Program Assignments/src/pa2/Token.java
+++ Program Assignments/src/pa2/Token.java
@@ -52,6 +52,65 @@ public final class Token {
 			this.type = type;
 			this.data = data;
 		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Object#hashCode()
+		 */
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((data == null) ? 0 : data.hashCode());
+			result = prime * result + ((type == null) ? 0 : type.hashCode());
+			return result;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Object#equals(java.lang.Object)
+		 */
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Builder other = (Builder) obj;
+			if (data == null) {
+				if (other.data != null)
+					return false;
+			} else if (!data.equals(other.data))
+				return false;
+			if (type != other.type)
+				return false;
+			return true;
+		}
+
+		private Token build() {
+
+		}
+	}
+	public final class LocationalToken{
+		private final Token token;
+		private final int location;
+		/**
+		 * @return the token
+		 */
+		public Token getToken() {
+			return token;
+		}
+		/**
+		 * @return the location
+		 */
+		public int getLocation() {
+			return location;
+		}
+		
 	}
 
 }
