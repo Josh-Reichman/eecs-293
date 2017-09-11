b5a0bc9ba5166b874960581264d9a26a161fc31a
Josh Reichman <whodaman101@gmail.com>
2017-09-11 15:19:37 -0400

    Deleted Type.jave, added a lot of code

--- Program Assignments/src/pa2/Token.java
+++ Program Assignments/src/pa2/Token.java
@@ -13,11 +13,69 @@ public final class Token {
 	}
 
 	public static Token of(Type type, String data) {
+		return null; // TODO: replace with of() code
 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Token other = (Token) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+
+	@Override
+	public String toString() {
+		return "Token [type=" + type + ", data=" + data + ", toString()=" + super.toString() + "]";
+	}
+
 	public enum Type {
-		NOT, AND, OR, OPEN, CLOSE, ID, NUMBER, BINARYOF, WHITESPACE;
+		NOT("not",false),
+		AND("and",false),
+		OR("or",false), 
+		OPEN("(",false),
+		CLOSE(")",false), 
+		ID("[a-z]",true),
+		NUMBER("[\\d+]",true),
+		BINARYOP("[\\+\\-\\*\\/]",true),
+		WHITESPACE("\\s",false); 
 
 		private final String pattern;
 		private final Boolean hasData;
@@ -28,15 +86,14 @@ public final class Token {
 		}
 
 		/**
-		 * @return the pattern
+		 * @return pattern
 		 */
-
 		private String getPattern() {
 			return pattern;
 		}
 
 		/**
-		 * @return the hasData
+		 * @return hasData
 		 */
 		private Boolean getHasData() {
 			return hasData;
@@ -92,25 +149,34 @@ public final class Token {
 		}
 
 		private Token build() {
+			return null; // TODO: Replace with build() code
 
 		}
 	}
-	public final class LocationalToken{
+
+	public final class LocationalToken {
 		private final Token token;
-		private final int location;
+		private int location;
+
+		private LocationalToken(Token token, int location) {
+			this.token = token;
+			this.location = location;
+		}
+
 		/**
 		 * @return the token
 		 */
 		public Token getToken() {
 			return token;
 		}
+
 		/**
 		 * @return the location
 		 */
 		public int getLocation() {
 			return location;
 		}
-		
+
 	}
 
 }
