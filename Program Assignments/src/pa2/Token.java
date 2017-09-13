6ee65004ad76d0fad5ba9d5c27dfc79e84e00f8e
Josh Reichman <whodaman101@gmail.com>
2017-09-12 22:03:47 -0400

    More Updates

--- Program Assignments/src/pa2/Token.java
+++ Program Assignments/src/pa2/Token.java
@@ -23,7 +23,7 @@ public final class Token {
 			}
 			tokenMap.put(tokenBuilder, tokenBuilder.build());
 		}
-		//if builder exists but has a token mismatched, rebuild and replace
+		//if builder exists but token mismatched, rebuild and replace
 		Token token = new Token(type,Optional.ofNullable(data));
 		if(!tokenMap.get(tokenBuilder).equals(token)) {
 			tokenMap.replace(tokenBuilder, tokenBuilder.build());
@@ -75,46 +75,55 @@ public final class Token {
 	public String toString() {
 		return "Token [type=" + type + ", data=" + data + ", toString()=" + super.toString() + "]";
 	}
-	
-	//Type enum
-	public enum Type {
-		NOT("not", false), AND("and", false), OR("or", false), OPEN("(", false), CLOSE(")", false), ID("[a-z]+",
-				true), NUMBER("\\-?[0-9]", true), BINARYOP("\\+|-|\\*|/", true), WHITESPACE("\\s+", false);
 
+
+	//enum Type
+	public enum Type {
+		NOT("not", false),
+		AND("and", false), 
+		OR("or", false), 
+		OPEN("(", false), 
+		CLOSE(")", false),
+		ID("[a-z]+",true), 
+		NUMBER("\\-?[0-9]", true), 
+		BINARYOP("\\+|-|\\*|/", true),
+		WHITESPACE("\\s+", false);
+	
 		private final String pattern; //Indicates the regex pattern in a token
 		private final Boolean hasData; //Indicates the presence of ancillary data in a token
-
-		private Type(String pattern, Boolean hasData) {
+	
+		Type(String pattern, Boolean hasData) {
 			this.pattern = pattern;
 			this.hasData = hasData;
 		}
-
+	
 		public String getPattern() {
 			return pattern;
 		}
-
+	
 		public Boolean getHasData() {
 			return hasData;
 		}
-
-	}
 	
+	}
+
+
 	//Token Builder Class
 	private static class Builder {
 		
 		private final Type type;
 		private final Optional<String> data;
-
+	
 		private Builder(Type type, Optional<String> data) {
 			this.type = type;
 			this.data = data;
 		}
-
+	
 		private Token build() {
 			return new Token(type, data);
 		}
-
-
+	
+	
 		@Override
 		public int hashCode() {
 			final int prime = 31;
@@ -123,7 +132,7 @@ public final class Token {
 			result = prime * result + ((type == null) ? 0 : type.hashCode());
 			return result;
 		}
-
+	
 		@Override
 		public boolean equals(Object obj) {
 			if (this == obj)
@@ -142,22 +151,24 @@ public final class Token {
 				return false;
 			return true;
 		}
-
+	
 	}
+
+
 	//LocationalToken Class
 	public final class LocationalToken {
 		private final Token token;
 		private int location;
-
+	
 		private LocationalToken(Token token, int location) {
 			this.token = token;
 			this.location = location;
 		}
-
+	
 		public Token getToken() {
 			return token;
 		}
-
+	
 		public int getLocation() {
 			return location;
 		}
@@ -165,11 +176,11 @@ public final class Token {
 		public Token.Type getTokenType() {
 	        return token.getType();
 	    }
-
+	
 	    public Optional<String> getTokenData() {
 	        return token.getData();
 	    }
-
+	
 	}
 
 }
---
+++ Program Assignments/src/pa2/TokenTest.java
@@ -0,0 +1,18 @@
+package pa2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenTest {
+    @org.junit.jupiter.api.Test
+    void of() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getData() {
+    }
+
+}
